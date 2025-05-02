995c21925b416dedbb588612afe39b6e08e30652
catlilface <aleklim98@gmail.com>
2025-05-02 08:04:45 +0300

    right bar fine tune

--- hypr/wallpapers.sh
+++ hypr/wallpapers.sh
@@ -4,11 +4,12 @@ WALLPAPER_DIR="$HOME/Pictures/Wallpapers/"
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 # Get a random wallpaper that is not the current one
-WALLPAPER1=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
-WALLPAPER2=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+WALLPAPER1=$(find "$WALLPAPER_DIR/right" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+WALLPAPER2=$(find "$WALLPAPER_DIR/left" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
 hyprctl hyprpaper preload "$WALLPAPER1"
 hyprctl hyprpaper preload "$WALLPAPER2"
+
 # Apply the selected wallpaper
 hyprctl hyprpaper wallpaper "DP-1,$WALLPAPER1"
 hyprctl hyprpaper wallpaper "HDMI-A-1,$WALLPAPER2"
