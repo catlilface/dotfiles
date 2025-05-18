9b8d4252e17bcbc8e24ae9db95895305b4cf1471
catlilface <aleklim98@gmail.com>
2025-05-18 18:14:32 +0300

    hehe

--- hypr/wallpapers.sh
+++ hypr/wallpapers.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-WALLPAPER_DIR="$HOME/.config/hypr/wallpapers"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+INDEX=$RANDOM
+
+WALLPAPER_URL="https://bing.biturl.top/?resolution=1920&format=image&index=random&mkt=random"
+
+wget -O "/tmp/$INDEX-left.jpg" "$WALLPAPER_URL"
+wget -O "/tmp/$INDEX-right.jpg" "$WALLPAPER_URL"
 
 # Get a random wallpaper that is not the current one
-WALLPAPER1=$(find "$WALLPAPER_DIR/right" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
-WALLPAPER2=$(find "$WALLPAPER_DIR/left" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+WALLPAPER1="/tmp/$INDEX-left.jpg"
+WALLPAPER2="/tmp/$INDEX-right.jpg"
 
-hyprctl hyprpaper preload "$WALLPAPER1"
-hyprctl hyprpaper preload "$WALLPAPER2"
-
-# Apply the selected wallpaper
-hyprctl hyprpaper wallpaper "DP-1,$WALLPAPER1"
-hyprctl hyprpaper wallpaper "HDMI-A-1,$WALLPAPER2"
+swww img -o "DP-1" "$WALLPAPER1"
+swww img -o "HDMI-A-1" "$WALLPAPER2"
